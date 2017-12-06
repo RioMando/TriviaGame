e1e4e34fa19cdbcacce75d777aa54d94de0bd0f5
Armando <roblesrioarmand@gmail.com>
2017-12-05 23:31:26 -0600

    dynamic assignation of response options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,16 @@
-$('#containerId').append('<input type="checkbox" name="myCheckbox" />');
+//$('#containerId').append('<input type="checkbox" name="myCheckbox" />');
+
+  // Loops through the array of responses
+  for (var i = 0; i < responses.one.length; i++) {
+    // Then dynamicaly generates buttons for each response option in the array
+    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    var a = $("<button>");
+    // Adds a class of response option to our checkbox
+    a.addClass("response");
+    // Added a data-attribute
+    a.attr("option", responses.one[i]);
+    // Provided the text asociated to the response
+    a.text(responses.one[i]);
+    // Added the button to the buttons-view div
+    $("#bcheckbox-view").append(a);
+  }
