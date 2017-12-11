7954c631b1e0b1e4628f3cb014a91b8649ad5f28
Armando <roblesrioarmand@gmail.com>
2017-12-11 00:47:08 -0600

    counting 1st question for correct and incorrect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,83 +1,107 @@
-var questionAnswer = $("<div>");
-// function starter() {
-  var starting = $("<button>");
-  starting.text("Start");
-  starting.attr("href", "#");
-  starting.addClass("button");
-  questionAnswer.append(starting);
-  $('#display-data').append(questionAnswer);
+// This code will run as soon as the page loads
+window.onload = function() {
+  $("#starter").on("click", function() {
+    start();
+   //tenQuestions(); 
+  });
+  $("#answers-div").on("click", ".dataOption", function(){
+    name = $(this).attr("name");
+    compareAnswer(name);
+   //tenQuestions();
+  });
+};
 
-// }
-$(".button").on("click", function() {
+var correct = 0;
+var wrong = 0;
+var not_answered = 0; 
+var correctAnswer = ""; // To store the correct answer given by the Trivia API
+var i = 0;       // Variable used as index to display the 4 answer options
+var j = 0;       // Variable used as index to display the 10 questions.
+var result = []; // Variable to store the array obtanied for the Trivia API 
+var name = "";   // To store the attribute "name" of the options selected by the user
 
+// Generate the start button 
+var questionAnswer = $("<div>"); 
+var starting = $("<button>");
+starting.text("Start");
+starting.addClass("button");
+questionAnswer.append(starting);
+$('#starter').append(questionAnswer);
+
+//To make the call to the Trivia API and store the response in the variable result
+function start(){
   $.ajax({
     //url: "https://opentdb.com/api.php?amount=10&category=17&difficulty=easy&type=multiple",
     //url: "https://opentdb.com/api.php?amount=10&category=14&difficulty=easy&type=multiple",
     url: "https://opentdb.com/api.php?amount=10&category=32&difficulty=easy&type=multiple",  //Category: Cartoon & Animations
     method: "GET"
   }).done(function(resp) {
-    console.log(resp);
-    console.log("Answer: " + resp.results[0].correct_answer);
-    $("#answers-div").empty();
 
-    var result = resp.results;
-    number = Math.floor(Math.random() * 4);
-    console.log("number: " + number);
+//    $("#starter").empty();  ///////\\\\\\ To remove the initial Start button
+    result = resp.results;
+    tenQuestions(); //Call function to display the 1st question  /\/\/\/\/\
+}); // End of .done function
 
-    //Array and for-loop to store the 4 options icluding the correct option in a random place to be displayed  
-    displayValue(result[0].question, "question");  //Display Question on Screen
-    var optionAnswer = [];
+function tenQuestions() {
+  console.log("correct answer: " + result[j].correct_answer);
+  console.log("wrong answers: " + result[j].incorrect_answers);
+  //Array to go through the 10 questions returned by the API
+//  for (var j=0; j<10; j++) {
+//    if (j < 10) {    
+    var number = Math.floor(Math.random() * 4);
+      //  $("#answers-div").empty(); /////////////////<<<<<<<<<< 12/10/17 @ 22:27
+    correctAnswer = result[j].correct_answer;
+    //Array and for-loop store the correct answer in a random place to be displayed (stored ramdomly) in a 4 options array)
+    displayValue(result[j].question, "question");  //Display Question on Screen
+    var answersArray = []; //Array to store the 4 anwsers, including the correct one
     for (var i = 0; i < 4; i++ ) {
       if (i < number ) {
-        optionAnswer[i] = result[0].incorrect_answers[i];
+        answersArray[i] = result[j].incorrect_answers[i];
       } else if (i > number) {
-        optionAnswer[i] = result[0].incorrect_answers[i-1];
-      } else {
-        optionAnswer[i] = result[0].correct_answer;
+        answersArray[i] = result[j].incorrect_answers[i-1];
+      } else {                                     //Stores the correct answer in the random position of the array 
+        answersArray[i] = result[j].correct_answer;
       }
-        displayValue(optionAnswer[i], "dataOption"); //Call function to display information on screen
-    }
-  });
-
-  // var p = $("<P>").text("Time R`emaining: " + clock);
-  // here goes the FOR-LOOP
-  function displayValue(info, typeAttr) {
-  //  $(this).addClass("optns");//////////////////
-    var questionAnswer = $("<div>");
-    var z = info;
-    questionAnswer.addClass(typeAttr);
-    questionAnswer.attr("name", z);
-    var infoQuestion = info;
-    // var p = $("<p>");
-    // questionAnswer.append(p);
-    questionAnswer.append("<p>" + infoQuestion + "</p>");
-    
-    $('#answers-div').append(questionAnswer);
+      displayValue(answersArray[i], "dataOption"); //Call function to display information on screen
+    } // End of i-for-loop
+//      j++;
+//    } // End if j<10 condition
+//  } // End of tenQuestions function (j-for-loop) 
   }
-});
+}// End of tenQuestions function
 
-    // Adding a click event listener to all elements with a class of "movie"
-    //this is the good code      $("#answers-div").on("click", ".dataOption", calling );
-      $("#answers-div").on("click", ".dataOption", function(){
-        console.log($(this).attr("name"));
-      });
+function displayValue(info, typeAttr) {
+//  $(this).addClass("optns");//////////////////
+  var questionAnswer = $("<div>");
+  var z = info;
+  questionAnswer.addClass(typeAttr);
+  questionAnswer.attr("name", z);
+  var infoQuestion = info;
+  // var p = $("<p>");
+  // questionAnswer.append(p);
+  questionAnswer.append("<p>" + infoQuestion + "</p>");
+  $('#answers-div').append(questionAnswer);
+} // End displayValue function
 
-// $(".dataOption").click(function(event){
-//   event.preventDefault();
-// //  console.log(this); 
-// //  calling();
-// });
+function compareAnswer(nameHere){
+  
+  if (nameHere === correctAnswer) {
+    correct++;
+    console.log("corrects: " + correct);
+  } else {
+    wrong++;
+    console.log("wrong: " + wrong);
+  }
+  j++;
+//  $(".dataOption").empty();
+
+} // End of compareAnswer function
 
 
-function calling() {
-  console.log("You selected an answer");
-};
 
-/*
-  function EvaluateAnswer () {
-    alert("correct, #: " );
-    console.log(this.name);
-  }*/
+
+
+
 
   /* NEXT STEPS:
   - Compare attribute name from selected option with the correct answer.
